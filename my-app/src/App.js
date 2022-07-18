74520dd91cf480776d71987647f478c1c28d6483
maxmotsyk <21mechanic21@gmail.com>
2022-07-18 22:39:12 +0300

    commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,62 +3,7 @@ import './App.css';
 import Fcomponent from './Fcomponent';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Fcomponent></Fcomponent>
-        <img src={logo} className="App-logo" alt="logo"/>
-        <div>
-
-          <ul>  
-
-              <li>
-                <a></a>
-              </li>
-
-              <li>
-                <a></a>
-              </li>
-
-              <li>
-                <a></a>
-              </li>
-
-              <li>
-                <a></a>
-              </li>
-
-              <li>
-                <a></a>
-              </li>
-              
-              <li>
-                <a></a>
-              </li>
-              
-
-          
-
-          
-          </ul>
-          
-        </div>
-
-        <div>
-          
-        </div>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return ();
 }
 
 export default App;
